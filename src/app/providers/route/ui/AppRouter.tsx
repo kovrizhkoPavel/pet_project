107cd52b556fef8923da744a613f8dcbdf4b8838
pavel <kovrizhkopavel@gmail.com>
2024-03-04 21:39:45 +0300

    feat: add Loader

--- src/app/providers/route/ui/AppRouter.tsx
+++ src/app/providers/route/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
-import { FC, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { FC, Suspense } from 'react';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter: FC = () => (
-  <Suspense fallback={<div>Loading</div>}>
+  <Suspense fallback={<PageLoader />}>
     <Routes>
       {routeConfig.map(({ path, element }) => (
         <Route key={path} path={path} element={element} />
---
+++ src/widgets/PageLoader/ui/PageLoader.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { getClassName } from 'shared/lib/classNames/getClassName';
+import { Loader } from 'shared/ui/Loader/Loader';
+import cls from './PageLoader.module.scss';
+
+type TPageLoaderProps = {
+  className?: string;
+}
+
+export const PageLoader: FC<TPageLoaderProps> = ({ className }) => (
+  <div className={getClassName(cls.pageLoader, {}, [className])}>
+    <Loader />
+  </div>
+);
