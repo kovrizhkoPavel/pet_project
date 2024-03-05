78d3d9077cb22bb7eedccbdfd2f2d4ac62c5b496
pavel <kovrizhkopavel@gmail.com>
2024-03-05 23:43:31 +0300

    feat: update packages

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,16 +1,18 @@
-import {FC, useMemo, useState} from 'react';
-import {ThemeContext} from "../lib/ThemeContext";
-import {LOCAL_STORAGE_THEME_KEY, Theme} from "../constants";
+import {
+  FC, ReactNode, useMemo, useState,
+} from 'react';
+import { ThemeContext } from '../lib/ThemeContext';
+import { LOCAL_STORAGE_THEME_KEY, Theme } from '../constants';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({children}) => {
+const ThemeProvider: FC<{children: ReactNode}> = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(defaultTheme);
 
   const defaultProps = useMemo(() => ({
     theme,
     setTheme,
-  }), [theme])
+  }), [theme]);
 
   return (
     <ThemeContext.Provider value={defaultProps}>
