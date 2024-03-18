d4aef4b29e2d43f39189dc6e2645291653ef699c
pavel <kovrizhkopavel@gmail.com>
2024-03-18 14:47:26 +0300

    feat: create portal

---
+++ src/shared/ui/Portal/Portal.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+type TPortalProps = {
+  children: ReactNode;
+  domNode?: HTMLElement;
+}
+const Portal: FC<TPortalProps> = (props) => {
+  const { children, domNode = document.body } = props;
+  return (
+    createPortal(children, domNode)
+  );
+};
+
+export default Portal;
