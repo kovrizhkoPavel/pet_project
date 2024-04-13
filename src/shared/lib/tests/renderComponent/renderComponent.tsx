e671aabf2cc63546d8b79597ddbd66a0249a9edf
pavel <kovrizhkopavel@gmail.com>
2024-04-13 21:55:53 +0300

    fix: unit tests

--- src/shared/lib/tests/renderComponent/renderComponent.tsx
+++ src/shared/lib/tests/renderComponent/renderComponent.tsx
@@ -14,12 +14,13 @@ export const renderComponent = (component: ReactNode, options: TOptions = {}) =>
   const { route = '/', initialState } = options;
 
   return render(
-    <StoreProvider initialState={initialState}>
-      <MemoryRouter initialEntries={[route]}>
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider initialState={initialState}>
         <I18nextProvider i18n={i18nForTest}>
           {component}
         </I18nextProvider>
-      </MemoryRouter>
-    </StoreProvider>,
+      </StoreProvider>
+    </MemoryRouter>,
+
   );
 };
