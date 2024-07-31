990b88a42e8e3cbfc04c7eb5c3187f18cc569735
pavel <kovrizhkopavel@gmail.com>
2024-07-31 13:01:35 +0300

    feat: add addCommentFormSlice

---
+++ src/features/AddCommentForm/model/slice/addCommentFormSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AddCommentFormScheme } from 'features/AddCommentForm/model/types/AddCommentFormScheme';
+
+const initialState: AddCommentFormScheme = {};
+
+export const addCommentFormSlice = createSlice({
+  name: 'addCommentForm',
+  initialState,
+  reducers: {
+    setText(state, action: PayloadAction<string>) {
+      state.text = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    // builder
+    //   .addCase(.pending, (state) => {
+    //   })
+    //   .addCase(.fulfilled, (state, action: PayloadAction<TArticle>) => {
+    //   })
+    //   .addCase(fetchArticleById.rejected, (state, action) => {
+    //   });
+  },
+});
+
+export const { actions: addCommentFormActions } = addCommentFormSlice;
+export const { reducer: addCommentFormReducer } = addCommentFormSlice;
