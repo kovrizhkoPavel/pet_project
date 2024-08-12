dbac729d38bc28d76fc53ca85cb153f34f8a9334
pavel <kovrizhkopavel@gmail.com>
2024-08-12 21:13:33 +0300

    feat: make Card

---
+++ src/shared/ui/Card/Card.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider/constants';
+import { Text } from 'shared/ui/Text/Text';
+import { Card } from './Card';
+
+const meta = {
+  title: 'shared/Card',
+  component: Card,
+  args: {
+    children: <Text title="title" text="text" />,
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {};
+
+export const Dark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
