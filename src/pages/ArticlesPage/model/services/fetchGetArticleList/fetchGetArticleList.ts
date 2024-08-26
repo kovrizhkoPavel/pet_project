b7593ec16a225415f9a4d441953e7c0fecf1f36a
pavel <kovrizhkopavel@gmail.com>
2024-08-26 21:49:39 +0300

    feat: add pageName to params

--- src/pages/ArticlesPage/model/services/fetchGetArticleList/fetchGetArticleList.ts
+++ src/pages/ArticlesPage/model/services/fetchGetArticleList/fetchGetArticleList.ts
@@ -2,20 +2,29 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { TThunkApiConfig } from 'shared/types/stateScheme';
 import { ArticleUrl } from 'shared/constants/api';
 import { TArticle } from 'entities/Article/model/types/article';
+import { getLimit } from '../../selectors/getArticles';
+
+type TFetchGetArticlesParams = {
+  pageNum: number;
+};
 
 export const fetchGetArticleList = createAsyncThunk<
   TArticle[],
-  void,
+  TFetchGetArticlesParams,
   TThunkApiConfig<string>
 >(
   'ArticlePage/fetchGetArticleList',
-  async (_, thinkAPI) => {
-    const { extra, rejectWithValue } = thinkAPI;
+  async ({ pageNum }, thinkAPI) => {
+    const { extra, rejectWithValue, getState } = thinkAPI;
+
+    const limit = getLimit(getState());
 
     try {
       const response = await extra.api.get<TArticle[]>(ArticleUrl.ARTICLE, {
         params: {
           _expand: 'user',
+          _limit: limit,
+          _page: pageNum,
         },
       });
 
