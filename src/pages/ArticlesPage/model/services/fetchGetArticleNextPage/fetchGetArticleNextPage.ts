fbb15aee3b691579731d6cb680b883ee42a02a99
pavel <kovrizhkopavel@gmail.com>
2024-08-26 21:48:54 +0300

    feat: make fetchGetArticleNextPage

---
+++ src/pages/ArticlesPage/model/services/fetchGetArticleNextPage/fetchGetArticleNextPage.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { TThunkApiConfig } from 'shared/types/stateScheme';
+import { fetchGetArticleList } from 'pages/ArticlesPage/model/services/fetchGetArticleList/fetchGetArticleList';
+import { articlePageActions } from 'pages/ArticlesPage/model/slice/articlePageSlice';
+import { getHasMore, getIsLoading, getPageNum } from '../../selectors/getArticles';
+
+export const fetchGetArticleNextPage = createAsyncThunk<
+  void,
+  void,
+  TThunkApiConfig<string>
+>(
+  '/fetchGetArticleNextPage',
+  async (_, thinkAPI) => {
+    const { dispatch, getState } = thinkAPI;
+
+    const hasMore = getHasMore(getState());
+    const isLoading = getIsLoading(getState());
+    const pageNum = getPageNum(getState()) + 1;
+
+    if (isLoading || !hasMore) return;
+
+    dispatch(fetchGetArticleList({ pageNum }));
+    dispatch(articlePageActions.setPageNum(pageNum));
+  },
+);
