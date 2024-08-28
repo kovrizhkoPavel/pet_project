3926c3d77f54b3dea61ca373ac9c1b63ba5be90e
pavel <kovrizhkopavel@gmail.com>
2024-08-28 12:47:35 +0300

    refactor: rename hook

---
+++ src/shared/lib/hooks/useIntersectionObserver.ts
@@ -0,0 +1,32 @@
+import { MutableRefObject, useEffect } from 'react';
+
+type TProps = {
+  rootRef: MutableRefObject<HTMLElement> | null | undefined;
+  observeRef: MutableRefObject<HTMLElement> | null | undefined;
+  cb: VoidFunction;
+}
+
+export const useIntersectionObserver = (props: TProps) => {
+  const { rootRef, observeRef, cb } = props;
+
+  useEffect(() => {
+    const observeElm = observeRef?.current;
+    const rootElm = rootRef?.current;
+    let observer: IntersectionObserver;
+
+    if (observeElm && rootElm) {
+      const options = {
+        root: rootElm,
+        rootMargin: '0px',
+        threshold: 1.0,
+      };
+
+      observer = new IntersectionObserver(cb, options);
+      observer.observe(observeElm);
+    }
+
+    return () => {
+      if (observer && observeElm) observer.unobserve(observeElm);
+    };
+  }, [rootRef, observeRef, cb]);
+};
