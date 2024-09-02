46cc465247dcb6c196576b6f081ecc87c0390e07
pavel <kovrizhkopavel@gmail.com>
2024-09-02 19:41:55 +0300

    feat: add AppLink

--- src/entities/Article/ui/ArticleList/CardSmall/CardSmall.tsx
+++ src/entities/Article/ui/ArticleList/CardSmall/CardSmall.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import { getClassName } from 'shared/lib/classNames/getClassName';
 import { Card } from 'shared/ui/Card/Card';
 import { TArticle } from 'entities/Article/model/types/article';
+import { RoutePath } from 'shared/config/routeConfig/constants';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
 import cls from './CardSmall.module.scss';
 import { ViewsCount } from '../ViewsCount/ViewsCount';
 
@@ -20,14 +22,16 @@ export const CardSmall: FC<TCardSmallProps> = ({ className, article }) => {
   } = article;
 
   return (
-    <Card className={getClassName(cls.cardSmall, {}, [className])}>
-      <img className={cls.img} src={img} alt="logo" />
-      <div className={cls.date}>{createdAt}</div>
-      <div className={cls.categoriesWrap}>
-        <div className={cls.categories}>{type.join(', ')}</div>
-        <ViewsCount count={views} />
-      </div>
-      <div>{title}</div>
-    </Card>
+    <AppLink to={`${RoutePath.article_details}${article.id}`}>
+      <Card className={getClassName(cls.cardSmall, {}, [className])}>
+        <img className={cls.img} src={img} alt="logo" />
+        <div className={cls.date}>{createdAt}</div>
+        <div className={cls.categoriesWrap}>
+          <div className={cls.categories}>{type.join(', ')}</div>
+          <ViewsCount count={views} />
+        </div>
+        <div>{title}</div>
+      </Card>
+    </AppLink>
   );
 };
