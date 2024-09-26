fa69da289116633719e716fbef1c3651858bc036
pavel <kovrizhkopavel@gmail.com>
2024-09-26 21:29:59 +0300

    feat: add interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,13 @@ import { LocalStorageKey } from 'shared/constants/localstorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(LocalStorageKey.AUTH_DATA) || '',
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    // eslint-disable-next-line no-param-reassign
+    config.headers.Authorization = localStorage.getItem(LocalStorageKey.AUTH_DATA) || '';
+  }
+
+  return config;
 });
