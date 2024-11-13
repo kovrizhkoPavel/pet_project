ac8fa77c95ee5d759965abeb90d0649f9feafe74
pavel <kovrizhkopavel@gmail.com>
2024-11-13 16:12:34 +0300

    feat: add checker plugin

--- eslint-plugin-pet-project/rules/fsd-relative-path.js
+++ eslint-plugin-pet-project/rules/fsd-relative-path.js
@@ -1,21 +1,58 @@
+'use strict';
+
+const path = require('path');
+
+const layers = {
+  'entities': 'entities',
+  'features': 'features',
+  'shared': 'shared',
+  'pages': 'pages',
+  'widgets': 'widgets',
+}
+
 module.exports = {
   meta: {
     type: null,
     docs: {
       description: 'feature sliced relative path checker',
-      category: 'Fill me in',
       recommended: false,
-      url: null, // URL to the documentation page for this rule
     },
     fixable: null, // Or `code` or `whitespace`
     schema: [], // Add a schema if the rule has options
   },
 
-  create(content) {
+  create(context) {
     return {
-      Literal() {
-        console.log('ffdf');
-      },
+      ImportDeclaration(node) {
+        // widgets/PageLoader
+        const importPath = node.source.value;
+        
+        // /Users/user/project/pet_project/src/app/providers/route/ui/AppRouter.tsx
+        const filePath = context.filename;
+        
+        if (checkIsPathInvalid(filePath, importPath)) {
+          context.report(node, 'Within a single slice, all paths must be relative')
+        }
+      }
     };
   },
 };
+
+function checkIsPathInvalid(from, to) {
+  if (checkIsPathRelative(to)) return false;
+  
+  const [importedLayer, importedSlice] = to.split('/');
+  
+  if (!importedLayer in layers) return false;
+  
+  const [,normalizedPath] = path.toNamespacedPath(from).split('src'); // '/entities/Article/ui/ArticleInfo/ArticleInfo.tsx'
+  const [,layer, slice] = normalizedPath.split(path.sep); // [ '', 'entities', 'Article', 'ui', 'ArticleInfo', 'ArticleInfo.tsx' ]
+  
+  if (!layer || !slice) return false;
+  
+  return importedLayer === layer && importedSlice === slice
+}
+
+function checkIsPathRelative(path) {
+  return path === '.' || path.startsWith('./') || path.startsWith('../');
+}
