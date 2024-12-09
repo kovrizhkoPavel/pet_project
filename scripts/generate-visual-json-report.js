1df35188aa2141ecf9af85ed55c312b941a40d9e
pavel <kovrizhkopavel@gmail.com>
2024-12-09 20:59:55 +0300

    refactor: rename vars

--- scripts/generate-visual-json-report.js
+++ scripts/generate-visual-json-report.js
@@ -3,7 +3,7 @@ const { readdir, writeFile } = require('fs');
 const { join: joinPath, relative } = require('path');
 
 const asyncReaddir = promisify(readdir);
-const writeFileAsync = promisify(writeFile);
+const asyncWriteFile = promisify(writeFile);
 
 const lokiDir = joinPath(__dirname, '..', '.loki');
 const actualDir = joinPath(lokiDir, 'current');
@@ -11,18 +11,18 @@ const expectedDir = joinPath(lokiDir, 'reference');
 const diffDir = joinPath(lokiDir, 'difference');
 
 (async function main() {
-    const diffs = await asyncReaddir(diffDir);
+  const diffs = await asyncReaddir(diffDir);
 
-    await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
-        newItems: [],
-        deletedItems: [],
-        passedItems: [],
-        failedItems: diffs,
-        expectedItems: diffs,
-        actualItems: diffs,
-        diffItems: diffs,
-        actualDir: relative(lokiDir, actualDir),
-        expectedDir: relative(lokiDir, expectedDir),
-        diffDir: relative(lokiDir, diffDir),
-    }));
+  await asyncWriteFile(joinPath(lokiDir, 'report.json'), JSON.stringify({
+    newItems: [],
+    deletedItems: [],
+    passedItems: [],
+    failedItems: diffs,
+    expectedItems: diffs,
+    actualItems: diffs,
+    diffItems: diffs,
+    actualDir: relative(lokiDir, actualDir),
+    expectedDir: relative(lokiDir, expectedDir),
+    diffDir: relative(lokiDir, diffDir),
+  }));
 }());
