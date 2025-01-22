14f231e981a31044f988239dbf0cba64b6f6a7ba
pavel <kovrizhkopavel@gmail.com>
2025-01-22 21:43:06 +0300

    feat: add babel loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,26 +1,24 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { TBuildOptions } from './types/config';
+import {TBuildOptions} from './types/config';
 import {buildCssLoader} from "./loaders/buildCssLoader";
 import {buildSvgLoader} from "./loaders/buildSvgLoader";
 import {buildBabelLoader} from "./loaders/buildBabelLoader";
-import {buildTSLoader} from "./loaders/buildTSLoader";
 import {buildFileLoader} from "./loaders/buildFileLoader";
 
 export const buildLoader = (options: TBuildOptions): webpack.RuleSetRule[] => {
   const { isDev } = options;
 
-  const typeScriptLoader = buildTSLoader();
   const cssLoader = buildCssLoader(isDev);
   const svgLoader = buildSvgLoader();
-  const babelLoader = buildBabelLoader();
   const fileLoader = buildFileLoader();
+  const codeBabelLoader = buildBabelLoader({isDev, isTsx: false});
+  const tsxBabelLoader = buildBabelLoader({isDev, isTsx: true});
 
   return [
     fileLoader,
     svgLoader,
+    codeBabelLoader,
+    tsxBabelLoader,
     cssLoader,
-    babelLoader,
-    typeScriptLoader,
   ];
 };
--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,10 +1,23 @@
-export const buildBabelLoader = () => ({
-  test: /\.(tsx|jsx|js)$/,
+type TOptions = {
+  isDev: boolean,
+  isTsx?: boolean,
+}
+
+export const buildBabelLoader = ({isTsx, isDev}: TOptions) => ({
+  test: isTsx ? /\.(tsx|jsx)$/ : /\.(ts|js)$/,
   exclude: /node_modules/,
   use: {
     loader: 'babel-loader',
     options: {
       presets: ['@babel/preset-env'],
+      plugins: [
+        'i18next-extract',
+        '@babel/plugin-transform-runtime',
+        [
+          '@babel/plugin-transform-typescript',
+          {isDev}
+        ]
+      ],
     },
   },
 })
