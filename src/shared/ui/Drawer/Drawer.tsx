0b9970a51d40b1d5adbac26657bc26ac9ea01f46
pavel <kovrizhkopavel@gmail.com>
2025-02-12 21:56:13 +0300

    feat: add close style

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -2,28 +2,34 @@ import { getClassName } from 'shared/lib/classNames/getClassName';
 import { TStyleMod, TWithChildren } from 'shared/types/utils';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Overlay } from 'shared/ui/Overlay/Overlay';
+import { useModal } from 'shared/lib/hooks/useModal';
 import cls from './Drawer.module.scss';
 
 type TDrawerProps = {
   className?: string;
-  isOpen?: boolean;
-  onClose?: VoidFunction;
+  lazy?: boolean;
+  isOpen: boolean;
+  onClose: VoidFunction;
 } & TWithChildren
 
 export const Drawer = (props: TDrawerProps) => {
   const {
-    className, children, isOpen, onClose,
+    className, children, isOpen, lazy, onClose,
   } = props;
 
   const { theme } = useTheme();
+  const { isMounted, isClosing, closeHandler } = useModal({ isOpen, onClose });
+
+  if (lazy && !isMounted) return null;
 
   const mods: TStyleMod = {
-    [cls.opened]: !!isOpen,
+    [cls.opened]: isOpen,
+    [cls.closed]: isClosing,
   };
 
   return (
     <div className={getClassName(cls.drawer, mods, [className, theme])}>
-      <Overlay onclick={onClose} />
+      <Overlay onclick={closeHandler} />
       <div className={cls.content}>
         {children}
       </div>
