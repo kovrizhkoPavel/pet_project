3b4c657b2b3d3465b551ea32783fbb9169a78614
pavel <kovrizhkopavel@gmail.com>
2025-02-12 15:31:15 +0300

    feat: add animation to modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,33 +1,59 @@
-import {
-  FC, ReactNode, useCallback, useEffect, useState,
-} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getClassName } from 'shared/lib/classNames/getClassName';
 import { KeyboardKey } from 'shared/constants/common';
 import Portal from 'shared/ui/Portal/Portal';
 import { Overlay } from 'shared/ui/Overlay/Overlay';
+import { TWithChildren } from 'shared/types/utils';
 import cls from './Modal.module.scss';
 
 type TModalProps = {
-  children: ReactNode;
   isOpen: boolean;
   onClose: () => void;
   className?: string;
   contentClass?: string;
   title?: string;
   lazy?: boolean;
-}
+} & TWithChildren;
 
-export const Modal: FC<TModalProps> = (props) => {
+const ANIMATION_DELAY = 300;
+
+export const Modal = (props: TModalProps) => {
   const [isMounted, setIsMounted] = useState(false);
   const {
     className, contentClass, title, children, isOpen, lazy, onClose,
   } = props;
 
+  const [isClosing, setIsClosing] = useState(false);
+
+  const closeHandler = useCallback(() => {
+    if (!onClose) return;
+
+    setIsClosing(true);
+
+    const startTime = performance.now(); // Время начала анимации
+
+    function animationLoop() {
+      const currentTime = performance.now();
+
+      // Проверяем, прошло ли достаточно времени для завершения анимации
+      if (currentTime - startTime >= ANIMATION_DELAY) {
+        onClose();
+        setIsClosing(false);
+      } else {
+        // Продолжаем анимационный цикл
+        requestAnimationFrame(animationLoop);
+      }
+    }
+
+    // Начинаем анимационный цикл
+    requestAnimationFrame(animationLoop);
+  }, [onClose]);
+
   const onKeyDown = useCallback((evt: KeyboardEvent) => {
     if (evt.key === KeyboardKey.ESCAPE) {
-      onClose();
+      closeHandler();
     }
-  }, [onClose]);
+  }, [closeHandler]);
 
   useEffect(() => {
     document.addEventListener('keydown', onKeyDown);
@@ -45,13 +71,18 @@ export const Modal: FC<TModalProps> = (props) => {
 
   if (!isMounted && lazy) return null;
 
+  const mods = {
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
+  };
+
   return (
     <Portal>
       <div className={
-        getClassName(cls.modal, { [cls.opened]: isOpen }, [className])
+        getClassName(cls.modal, mods, [className])
       }
       >
-        <Overlay onclick={onClose} />
+        <Overlay onclick={closeHandler} />
         <div className={cls.content}>
           {title && <h4>{title}</h4>}
           <div className={getClassName('', {}, [contentClass])}>
