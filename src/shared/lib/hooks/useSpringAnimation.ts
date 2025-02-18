7f054c8fd0a67ce20dd836ee1037efecff0d18e8
pavel <kovrizhkopavel@gmail.com>
2025-02-18 22:13:47 +0300

    refactor: make useSpringAnimation

---
+++ src/shared/lib/hooks/useSpringAnimation.ts
@@ -0,0 +1,67 @@
+import { useAnimationLibs } from 'shared/lib/Providers/AnimationProvider';
+import { useCallback, useEffect } from 'react';
+import { ANIMATION_DURATION } from 'shared/constants/assets';
+
+type TProps = {
+  isOpen: boolean;
+  onClose: VoidFunction;
+}
+
+export const useAnimation = (props: TProps) => {
+  const height = window.innerHeight;
+  const { onClose, isOpen } = props;
+  const {
+    Gesture: { useDrag },
+    Spring: { a, config, useSpring },
+  } = useAnimationLibs();
+
+  const [{ y }, api] = useSpring(() => ({ y: height }));
+
+  const openHandler = useCallback(() => {
+    api.start({ y: 0, immediate: false });
+  }, [api]);
+
+  const closeHandler = (velocity = 0) => {
+    api.start({ y: height, immediate: false, config: { ...config.stiff, velocity } });
+    setTimeout(() => onClose(), ANIMATION_DURATION);
+  };
+
+  const bind = useDrag(
+    ({
+      last,
+      velocity: [, vy],
+      direction: [, dy],
+      offset: [, oy],
+      cancel,
+    }) => {
+      if (oy < -70) cancel();
+
+      if (!last) openHandler();
+
+      if (oy > height * 0.5 || (vy > 0.5 && dy > 0)) {
+        closeHandler(vy);
+        return;
+      }
+
+      openHandler();
+    },
+    {
+      from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
+    },
+  );
+
+  const display = y.to((py) => (py < height ? 'block' : 'none'));
+
+  useEffect(() => {
+    if (!isOpen) return;
+    openHandler();
+  }, [isOpen, openHandler]);
+
+  return {
+    a,
+    y,
+    closeHandler,
+    bind,
+    display,
+  };
+};
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -3,9 +3,8 @@ import { getClassName } from 'shared/lib/classNames/getClassName';
 import { TStyleMod, TWithChildren } from 'shared/types/utils';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Overlay } from 'shared/ui/Overlay/Overlay';
-import { useModal } from 'shared/lib/hooks/useModal';
-import { useCallback, useEffect } from 'react';
 import { useAnimationLibs } from 'shared/lib/Providers/AnimationProvider';
+import { useAnimation } from 'shared/lib/hooks/useSpringAnimation';
 import cls from './Drawer.module.scss';
 
 type TDrawerProps = {
@@ -19,70 +18,23 @@ const height = window.innerHeight;
 
 const DrawerContent = (props: TDrawerProps) => {
   const {
-    className, children, isOpen, lazy, onClose,
+    className, children, isOpen, onClose,
   } = props;
 
-  const { isMounted, isClosing, closeHandler } = useModal({ isOpen, onClose });
   const {
-    Gesture: { useDrag },
-    Spring: { a, config, useSpring },
-  } = useAnimationLibs();
-
-  const [{ y }, api] = useSpring(() => ({ y: height }));
-
-  const openDrawer = useCallback(() => {
-    api.start({ y: 0, immediate: false });
-  }, [api]);
-
-  const close = (velocity = 0) => {
-    api.start({ y: height, immediate: false, config: { ...config.stiff, velocity } });
-    closeHandler();
-  };
-
-  useEffect(() => {
-    if (!isOpen) return;
-    openDrawer();
-  }, [isOpen, openDrawer]);
-
-  const bind = useDrag(
-    ({
-      last,
-      velocity: [, vy],
-      direction: [, dy],
-      offset: [, oy],
-      cancel,
-    }) => {
-      if (oy < -70) cancel();
-
-      if (!last) openDrawer();
-
-      if (oy > height * 0.5 || (vy > 0.5 && dy > 0)) {
-        close(vy);
-        return;
-      }
-
-      openDrawer();
-    },
-    {
-      from: () => [0, y.get()], filterTaps: true, bounds: { top: 0 }, rubberband: true,
-    },
-  );
+    closeHandler, display, a, y, bind,
+  } = useAnimation({ isOpen, onClose });
 
   const { theme } = useTheme();
 
-  if (lazy && !isMounted) return null;
-
-  const display = y.to((py) => (py < height ? 'block' : 'none'));
-
   const mods: TStyleMod = {
     [cls.opened]: isOpen,
-    [cls.closed]: isClosing,
   };
 
   return (
     <div className={getClassName(cls.drawer, mods, [className, theme])}>
       <Overlay onclick={() => {
-        close();
+        closeHandler();
       }}
       />
       <a.div className={cls.content} {...bind()} style={{ display, bottom: `calc(-100vh + ${height}px)`, y }}>
